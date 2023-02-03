b927e0a899028df3c38979b08f3b7a233025a745
iftekharul-alam <116473747+iftekharul-alam@users.noreply.github.com>
2023-02-03 23:34:42 +0600

    signup work added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { FlexModule } from '@angular/flex-layout';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { CurrentTrainingComponent } from './training/current-training/current-training.component';
@@ -11,6 +14,8 @@ import { NewTrainingComponent } from './training/new-training/new-training.compo
 import { PastTrainingComponent } from './training/past-training/past-training.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { TrainingComponent } from './training/training.component';
+import { MaterialModule } from './material.module';
+
 
 @NgModule({
   declarations: [
@@ -26,7 +31,10 @@ import { TrainingComponent } from './training/training.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MaterialModule,
+    FlexModule
   ],
   providers: [],
   bootstrap: [AppComponent]
